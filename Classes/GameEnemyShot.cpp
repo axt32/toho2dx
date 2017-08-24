1b057636b5372971e7881fa69f5f08f23cb33405
axt32 <axt32@naver.com>
2017-08-24 12:37:57 +0900

    총알 자동삭제 안되던 버그 수정

--- Classes/GameEnemyShot.cpp
+++ Classes/GameEnemyShot.cpp
@@ -2,10 +2,11 @@
 
 GameEnemyShot::GameEnemyShot()
 {
-
+	m_bInsideCheck = false;
+	m_bBoundaryCheck = true;
 }
 
 GameEnemyShot::~GameEnemyShot()
 {
-
+	CCLOG("Game Enemy Shot Deleted");
 }
--- Classes/GamePlayerShot.cpp
+++ Classes/GamePlayerShot.cpp
@@ -7,5 +7,5 @@ GamePlayerShot::GamePlayerShot()
 }
 GamePlayerShot :: ~GamePlayerShot()
 {
-	
+
 }
